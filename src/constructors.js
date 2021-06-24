ecd3ad3484e9bf949538522d7c81bf11a245ee69
Keith Watford <keith.l.watford@gmail.com>
2021-06-24 19:40:36 -0400

    created some constructors that will probably be restructered later,  starting inquire.prompt logic

---
+++ src/constructors.js
@@ -0,0 +1,60 @@
+function Employee(name, id, email) {
+    this.name = name;
+    this.id = id;
+    this.email = email
+};
+
+getName();
+getId();
+getEmail();
+// getRole()—returns 'Employee'
+getRole(); 
+
+// -------------------
+
+function Manager(name, id, email, officeNumber) {
+    this.name = name;
+    this.id = id;
+    this.email = email
+    this.officeNumber = officeNumber;
+};
+
+getName();
+getId();
+getEmail();
+// getRole()—returns 'Manager'
+getRole(); 
+
+// -------------------
+
+function Engineer(name, id, email, github) {
+    this.name = name;
+    this.id = id;
+    this.email = email;
+    this.github = github;
+};
+
+getName();
+getId();
+getEmail();
+// getRole()—returns 'Engineer'
+getRole(); 
+getGithub()
+
+// -------------------
+
+function Intern(name, id, email, school) {
+    this.name = name;
+    this.id = id;
+    this.email = email;
+    this.school = school;
+};
+
+getName();
+getId();
+getEmail();
+// getRole()—returns 'Employee'
+getRole(); 
+getSchool()
+
+// -------------------
