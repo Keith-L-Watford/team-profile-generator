93c90df7f386d5cd3e9e6faf0e38ddf6c8e60ad5
Keith Watford <keith.l.watford@gmail.com>
2021-06-29 15:42:57 -0400

    tests werent working becuase i didnt install tests. so i did that. wrote two tests.

---
+++ tests/engineer.test.js
@@ -0,0 +1,19 @@
+const Engineer = require("../lib/engineer");
+
+
+ describe('Engineer', () => {
+    describe('init', () => {
+        it('should create new employee object if given valid arguments', () => {
+            //  Arrange
+            const testEngineer = new Engineer ("Barbara", 287, "email@email.com", "username" )
+            //  Act
+            //  Assert
+            expect(testEngineer.name).toEqual("Barbara");
+            expect(testEngineer.id).toEqual(287);
+            expect(testEngineer.email).toEqual("email@email.com");
+            expect(testEngineer.special).toEqual("username");
+         });
+        
+    });
+     
+ });
