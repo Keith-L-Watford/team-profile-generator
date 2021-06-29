3ce81033ab7660e4d881672440218912ff8af5a4
Keith Watford <keith.l.watford@gmail.com>
2021-06-28 20:31:45 -0400

    working on a test

--- tests/employee.test.js
+++ tests/employee.test.js
@@ -1,1 +1,18 @@
- const index = require("../index")
+const Employee = require("../lib/employee");
+
+
+ describe('Employee', () => {
+    describe('init', () => {
+        it('should create new employee object if given valid arguments', () => {
+            //  Arrange
+            const testEmployee = new Employee ("Kevin", 999, "email@email.com")
+            //  Act
+            //  Assert
+            expect(testEmployee.name).toEqual("Kevin");
+            expect(testEmployee.id).toEqual(999);
+            expect(testEmployee.email).toEqual("email@email.com");
+         });
+        
+    });
+     
+ });
